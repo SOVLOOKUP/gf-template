1750b1e4588d865a06325aaf9e48b04e7e9b4307
sovlookup <805408477@qq.com>
2020-11-12 22:49:46 +0800

    add gtoken

--- middleware/base.go
+++ middleware/base.go
@@ -6,10 +6,24 @@ import (
 	"net/http"
 )
 
+//请求拦截器
 func MiddlewareErrorHandler(r *ghttp.Request) {
 	r.Middleware.Next()
 	if r.Response.Status == http.StatusNotFound {
 		r.Response.ClearBuffer()
 		r.Response.WriteJsonExit(resp.NotFound("NoDataFound",""))
 	}
+}
+
+//鉴权登录
+func Login(r *ghttp.Request) (string, interface{}) {
+	username := r.GetString("username")
+	passwd := r.GetString("passwd")
+
+	//仅供测试
+	username = "sss"
+	userdata := passwd
+	// TODO 进行登录校验
+
+	return username, userdata
 }
